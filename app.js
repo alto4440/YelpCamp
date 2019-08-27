7eb06168bbac6be0b8869a5f0d38d5e12f35178a
root <alto4440@mylaurier.ca>
2019-08-27 14:04:55 +0000

    Changed app.listen to fix crash error

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() { 
-  console.log('Server listening on port 3000'); 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
